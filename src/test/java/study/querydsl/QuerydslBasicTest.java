3953d7a120fde2ab395fd559e30decce4a31213d
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-14 16:37:58 +0900

    feat: (QuerydslBasicTest) fetch별 쿼리 비교 테스트

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -83,5 +84,30 @@ public class QuerydslBasicTest {
             .fetchOne();
         assertThat(findMember.getUsername()).isEqualTo("member1");
         assertThat(findMember.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    public void resultFetch(){
+//        List<Member> fetch = queryFactory
+//            .selectFrom(member)
+//            .fetch();
+//
+//        Member fetchOne = queryFactory
+//            .selectFrom(member)
+//            .fetchOne();
+//
+//        Member fetchFirst = queryFactory
+//            .selectFrom(member)
+//            .fetchFirst();
+
+        QueryResults<Member> results = queryFactory
+            .selectFrom(member)
+            .fetchResults();
+        results.getTotal();
+        List<Member> content = results.getResults();
+
+        queryFactory
+            .selectFrom(member)
+            .fetchCount();
     }
 }
