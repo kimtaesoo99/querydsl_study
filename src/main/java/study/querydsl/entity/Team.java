2e94e028594957b31e3a4dd9888dbab72887cdc5
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-14 15:47:46 +0900

    feat: (Tean) 팀 엔티티 추가

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,27 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","name"})
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new LinkedList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
