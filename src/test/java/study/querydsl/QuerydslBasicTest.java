28c7ca0396c2fde2c0f5bd381f91f77fa30ec1b9
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-14 15:53:22 +0900

    test: (QuerydslBasicTest) jpql을 이용한 단건 조회

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,49 @@
+package study.querydsl;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    @BeforeEach
+    public void beforeEach(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1",10,teamA);
+        Member member2 = new Member("member2",20,teamA);
+        Member member3 = new Member("member3",30,teamB);
+        Member member4 = new Member("member4",40,teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL(){
+        //member1을 찾아라
+        Member findMember = em.createQuery("select m from Member m where m.username =:username", Member.class)
+            .setParameter("username", "member1").getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
