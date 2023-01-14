44571b825f85f542c9d33b4a3797df61476febeb
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-14 17:08:41 +0900

    feat: (QuerydslBasicTest) 세타 join 테스트 추가

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -208,4 +208,20 @@ public class QuerydslBasicTest {
         assertThat(result).extracting("username")
             .containsExactly("member1", "member2");
     }
+
+    @Test
+    public void theta_join(){
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+            .select(member)
+            .from(member, team)
+            .where(member.username.eq(team.name))
+            .fetch();
+
+        assertThat(result).extracting("username")
+            .containsExactly("teamA","teamB");
+    }
 }
