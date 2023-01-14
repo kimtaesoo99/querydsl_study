9b731539fa5d4c2d65ef2b4c549ca87061c8ca42
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-14 16:42:22 +0900

    feat: (QuerydslBasicTest) 정렬 테스트 추가

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -110,4 +110,26 @@ public class QuerydslBasicTest {
             .selectFrom(member)
             .fetchCount();
     }
+
+    @Test
+    public void sort(){
+        em.persist(new Member(null,100));
+        em.persist(new Member("member5",100));
+        em.persist(new Member("member6",100));
+
+        List<Member> result = queryFactory
+            .selectFrom(member)
+            .where(member.age.eq(100))
+            .orderBy(member.age.desc(), member.username.asc().nullsLast())
+            .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+
+    }
 }
