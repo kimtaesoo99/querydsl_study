11e4f39dc53cd0ac4a7f51e40c2e5141d3fed3bf
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-15 15:45:25 +0900

    test: (MemberSearchCondition) 검색조건 추가

---
+++ src/main/java/study/querydsl/dto/MemberSearchCondition.java
@@ -0,0 +1,14 @@
+package study.querydsl.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberSearchCondition {
+    //회원명, 팀명, 나이
+    private String username;
+    private String teamName;
+    private Integer ageGoe;
+    private Integer ageLoe;
+
+
+}
