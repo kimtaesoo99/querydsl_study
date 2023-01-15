e80e63755815945b5aca0844653d6e8683cb5ce5
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-15 13:08:18 +0900

    style: (QuerydslStudyApplicationTest) 의미없는 import 제거

--- src/test/java/study/querydsl/QuerydslStudyApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslStudyApplicationTests.java
@@ -1,9 +1,7 @@
 package study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
