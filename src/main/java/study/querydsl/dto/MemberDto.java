b94bb4754be85693492a944443db394832cc25eb
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-15 13:57:01 +0900

    feat: (MemberDto) @QueryProjection 어네테이션 추가

--- src/main/java/study/querydsl/dto/MemberDto.java
+++ src/main/java/study/querydsl/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package study.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -12,6 +13,7 @@ public class MemberDto {
     private String username;
     private int age;
 
+    @QueryProjection
     public MemberDto(String username, int age) {
         this.username = username;
         this.age = age;
