1899fbdfec6433db82d1685273c9ecc98c3e424f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-15 14:03:23 +0900

    feat: (QuerydslBasicTest) BooleanBuilder를 이용한 동적쿼리 테스트

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Expression;
@@ -492,5 +493,28 @@ public class QuerydslBasicTest {
         for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
         }
+    }
+
+    @Test
+    public void dynamicQuery_BooleanBuilder(){
+        String usernameParam = "member1";
+        Integer ageParam = null;
+
+        List<Member> result = searchMember1(usernameParam,ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond!=null){
+            builder.and(member.username.eq(usernameCond));
+        }
+        if (ageCond!=null){
+            builder.and(member.age.eq(ageCond));
+        }
+        return queryFactory
+            .selectFrom(member)
+            .where(builder)
+            .fetch();
     }
 }
