faa3c5ed2990d71c854f171ee60e8d2a3837c671
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-15 13:16:28 +0900

    feat: (QuerydslBasicTest) 생성자를 통한 DTO 조회

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -454,4 +454,15 @@ public class QuerydslBasicTest {
         }
     }
 
+    @Test
+    public void findDtoByConstructor() {
+        List<MemberDto> result = queryFactory
+            .select(Projections.constructor(MemberDto.class, member.username, member.age))
+            .from(member)
+            .fetch();
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+
 }
