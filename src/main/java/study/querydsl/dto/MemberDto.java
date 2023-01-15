9131af6bd25f9a4f92983da73fb8eda2a1e0ca7f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-15 13:08:36 +0900

    feat: (MemberDto) 멤버 DTO 추가

---
+++ src/main/java/study/querydsl/dto/MemberDto.java
@@ -0,0 +1,15 @@
+package study.querydsl.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+
+    private String username;
+    private int age;
+
+    public MemberDto(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
